913ffd78280c26d26cf71cb58f360f702af94bff
tayne3 <tayne3@dingtalk.com>
2025-06-19 15:48:29 +0800

    build: 🛠️ Refactor CMake configuration for project structure
    - Update CMakeLists.txt to use PROJECT_NAME for library definitions and include directories.
    - Modify OptionVariables.cmake to replace CUNIT_BUILD_* variables with PROJECT_* equivalents for better clarity.
    - Enhance ProjectConfig.cmake by introducing PROJECT_OUTPUT_DIR and PROJECT_BUILD_DEBUG for improved build management.
    - Streamline compile dependency handling and ensure consistent settings across different compilers.
    - This refactor improves maintainability and aligns the build system with project naming conventions.

--- cmake/ProjectConfig.cmake
+++ cmake/ProjectConfig.cmake
@@ -1,24 +1,12 @@
-add_library(cunit_compile_dependency INTERFACE)
-target_compile_features(cunit_compile_dependency INTERFACE c_std_99)
-
-string(TOLOWER "${CMAKE_SYSTEM_NAME}" SYSTEM_NAME)
-if(CUNIT_BUILD_DEBUG)
-	set(CUNIT_OUTPUT_DIR ${PROJECT_SOURCE_DIR}/bin/debug-${SYSTEM_NAME})
-	target_compile_definitions(cunit_compile_dependency INTERFACE -DDEBUG)
-else()
-	set(CUNIT_OUTPUT_DIR ${PROJECT_SOURCE_DIR}/bin/release-${SYSTEM_NAME})
-	target_compile_definitions(cunit_compile_dependency INTERFACE -DNDEBUG)
-endif()
-
-if(NOT CMAKE_RUNTIME_OUTPUT_DIRECTORY)
-	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CUNIT_OUTPUT_DIR})
-endif()
-if(NOT CMAKE_LIBRARY_OUTPUT_DIRECTORY)
-	set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CUNIT_OUTPUT_DIR})
-endif()
-# if(NOT CMAKE_ARCHIVE_OUTPUT_DIRECTORY)
-# 	set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CUNIT_OUTPUT_DIR})
-# endif()
+set(CMAKE_C_STANDARD 99)
+set(CMAKE_C_STANDARD_REQUIRED ON)
+set(CMAKE_C_EXTENSIONS ON)
+set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS ON)
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
 include(GNUInstallDirs)
 set(CMAKE_SKIP_BUILD_RPATH OFF)
@@ -39,38 +27,65 @@ if(PROJECT_IS_TOP_LEVEL)
 	endif()
 endif()
 
-set(CMAKE_C_STANDARD 99)
-set(CMAKE_C_STANDARD_REQUIRED ON)
-set(CMAKE_C_EXTENSIONS ON)
-set(CMAKE_CXX_STANDARD 11)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS ON)
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
-set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+add_library(${PROJECT_NAME}_compile_dependency INTERFACE)
+target_compile_features(${PROJECT_NAME}_compile_dependency INTERFACE c_std_99)
 
-# Set specific C language options for the MSVC compiler
+string(TOLOWER "${CMAKE_SYSTEM_NAME}" SYSTEM_NAME)
+if(PROJECT_BUILD_DEBUG)
+	set(PROJECT_OUTPUT_DIR ${PROJECT_SOURCE_DIR}/bin/debug-${SYSTEM_NAME})
+	target_compile_definitions(${PROJECT_NAME}_compile_dependency INTERFACE -DDEBUG)
+else()
+	set(PROJECT_OUTPUT_DIR ${PROJECT_SOURCE_DIR}/bin/release-${SYSTEM_NAME})
+	target_compile_definitions(${PROJECT_NAME}_compile_dependency INTERFACE -DNDEBUG)
+endif()
+
+if(NOT CMAKE_RUNTIME_OUTPUT_DIRECTORY)
+	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_OUTPUT_DIR})
+endif()
+if(NOT CMAKE_LIBRARY_OUTPUT_DIRECTORY)
+	set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_OUTPUT_DIR})
+endif()
+# if(NOT CMAKE_ARCHIVE_OUTPUT_DIRECTORY)
+# 	set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${PROJECT_OUTPUT_DIR})
+# endif()
+
+# set source charset to utf-8 for MSVC
 if(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
-    target_compile_options(cunit_compile_dependency INTERFACE 
-		"$<$<COMPILE_LANGUAGE:C>:/source-charset:utf-8>"
+    target_compile_options(${PROJECT_NAME}_compile_dependency INTERFACE 
+		"$<$<COMPILE_LANGUAGE:C>:/utf-8>"
+	)
+endif()
+if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    target_compile_options(${PROJECT_NAME}_compile_dependency INTERFACE 
+		"$<$<COMPILE_LANGUAGE:CXX>:/utf-8>"
 	)
 endif()
 
-# Set C language warning options for compilers other than MSVC and ClangCL (MSVC frontend).
-# These options help catch potential code issues.
+# compiler warnings, skipped for MSVC & ClangCL (MSVC frontend)
 if(NOT (CMAKE_C_COMPILER_ID STREQUAL "MSVC" OR ("${CMAKE_C_COMPILER_ID}" STREQUAL "Clang" AND "${CMAKE_C_COMPILER_FRONTEND_VARIANT}" STREQUAL "MSVC")))
-    target_compile_options(cunit_compile_dependency INTERFACE
+    target_compile_options(${PROJECT_NAME}_compile_dependency INTERFACE
         "$<$<COMPILE_LANGUAGE:C>:-Wall>"
         "$<$<COMPILE_LANGUAGE:C>:-Wextra>"
         "$<$<COMPILE_LANGUAGE:C>:-Werror=return-type>"
     )
 endif()
+if(NOT (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" OR ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" AND "${CMAKE_CXX_COMPILER_FRONTEND_VARIANT}" STREQUAL "MSVC")))
+    target_compile_options(${PROJECT_NAME}_compile_dependency INTERFACE
+        "$<$<COMPILE_LANGUAGE:CXX>:-Wall>"
+        "$<$<COMPILE_LANGUAGE:CXX>:-Wextra>"
+        "$<$<COMPILE_LANGUAGE:CXX>:-Werror=return-type>"
+    )
+endif()
 
-# Handle specific warning suppression options for older GCC versions (less than 6.0).
-# -Wno-missing-field-initializers is used to suppress warnings about incompletely initialized structs.
+# Handle -Wno-missing-field-initializers for older GCC
 if(CMAKE_C_COMPILER_ID STREQUAL "GNU" AND CMAKE_C_COMPILER_VERSION VERSION_LESS 6.0)
-    target_compile_options(cunit_compile_dependency INTERFACE 
+    target_compile_options(${PROJECT_NAME}_compile_dependency INTERFACE 
 		"$<$<COMPILE_LANGUAGE:C>:-Wno-missing-field-initializers>"
+	)
+endif()
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 6.0)
+    target_compile_options(${PROJECT_NAME}_compile_dependency INTERFACE 
+		"$<$<COMPILE_LANGUAGE:CXX>:-Wno-missing-field-initializers>"
 	)
 endif()
 
@@ -82,7 +97,7 @@ else()
     set(CUNIT_ROOT_PATH ${CMAKE_SOURCE_DIR})
     set(CUNIT_BUILD_PATH ${CMAKE_BINARY_DIR})
 endif()
-target_compile_definitions(cunit_compile_dependency INTERFACE
+target_compile_definitions(${PROJECT_NAME}_compile_dependency INTERFACE
     CUNIT_ROOT_PATH="${CUNIT_ROOT_PATH}"
     CUNIT_BUILD_PATH="${CUNIT_BUILD_PATH}"
 )
